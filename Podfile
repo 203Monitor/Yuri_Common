47266e811c1c535e856e8b9b02ca36b88466b5d9
yuri <930502900@qq.com>
2016-06-17 13:08:54 +0800

    new

--- Podfile
+++
@@ -1,9 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-
-platform:ios,'9.0'
-
-target "Yuri_Common" do
-
-  pod 'MBProgressHUD','~> 0.9.2'
-
-end
--- Pods/Target Support Files/Pods-Yuri_Common/Pods-Yuri_Common-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yuri_Common : NSObject
-@end
-@implementation PodsDummy_Pods_Yuri_Common
-@end
